150f588351383686af7736d4784ca1d15b795fce
Mauro Cezar Johann <maurojohann@gmail.com>
2020-12-16 01:43:02 -0300

    Configurador localização com google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:great_places/provider/greatPlaces.dart';
+import 'package:great_places/provider/great_Places.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
-
+import './screens/place_detail_screen.dart';
 import 'utils/app_routes.dart';
 
 void main() {
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
         routes: {
           AppRoutes.AUTH_HOME: (ctx) => PlaceListScreen(),
           AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen(),
+          AppRoutes.PLACE_DETAIL: (ctx) => PlaceDetailScreen(),
         },
       ),
     );
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:great_places/provider/greatPlaces.dart';
+import 'package:great_places/provider/great_Places.dart';
 import 'package:great_places/utils/app_routes.dart';
 import 'package:provider/provider.dart';
 
@@ -17,20 +17,33 @@ class PlaceListScreen extends StatelessWidget {
               }),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Text('Nenhum Local adicionado'),
-        ),
-        builder: (context, value, child) => value.itemsCount == 0
-            ? child
-            : ListView.builder(
-                itemCount: value.itemsCount,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                      backgroundImage: FileImage(value.itemByIndex(i).image)),
-                  title: Text(value.itemByIndex(i).title),
-                  onTap: () {},
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false).loadPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Text('Nenhum Local adicionado'),
                 ),
+                builder: (context, value, child) => value.itemsCount == 0
+                    ? child
+                    : ListView.builder(
+                        itemCount: value.itemsCount,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                              backgroundImage:
+                                  FileImage(value.itemByIndex(i).image)),
+                          title: Text(value.itemByIndex(i).title),
+                          subtitle: Text(value.itemByIndex(i).location.address),
+                          onTap: () {
+                            Navigator.of(context).pushNamed(
+                              AppRoutes.PLACE_DETAIL,
+                              arguments: value.itemByIndex(i),
+                            );
+                          },
+                        ),
+                      ),
               ),
       ),
     );
